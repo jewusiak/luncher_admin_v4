378108f7940bfbf18a10a06e5aa9f5132eca203f
Jewusiak Grzegorz <grzegorz@jewusiak.pl>
2024-07-25 13:15:18 +0200

    Updated gh workflow

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -1,5 +1,5 @@
 
 abstract class FFAppConstants {
-  static const String coreapibaseurl = 'https://core.api.pre1.luncher.pl';
+  static const String coreapibaseurl = 'https://api.pre.luncher.pl';
   static const String nullvalue = 'd6f35f82-da54-4473-b7a0-ab7b59cbafe7';
 }
